15b1869643cec38ace86c2e66d5b6cffc58ef58e
Juliien <julien1105@outlook.fr>
2022-05-01 02:55:54 +0200

    add: design animals

--- src/app/components/animals/animals-list/animals-list.component.ts
+++ src/app/components/animals/animals-list/animals-list.component.ts
@@ -10,19 +10,32 @@ export class AnimalsListComponent implements OnInit {
   isOpen: boolean = true;
   valueCols: number = 3;
   animals: any[] = [];
+  liked: number[] = [];
+  adoptionChecked: boolean = false;
 
   constructor(private animalService: AnimalsService) { }
 
   ngOnInit(): void {
     const storageValue = localStorage.getItem('valueCols');
-    if(storageValue) {
-        this.valueCols = parseInt(storageValue);
-    }
+    const storageLiked = localStorage.getItem('liked');
+    if(storageValue)
+      this.valueCols = parseInt(storageValue);
+
+    if(storageLiked)
+      this.liked = storageLiked.split(',').map(x => +x);
+
     this.animalService.getUserAnimals().subscribe(animals => this.animals = animals);
   }
 
   setValueColsToStorage(): number {
     localStorage.setItem('valueCols', this.valueCols.toString());
     return 0;
+  }
+
+  adopted(id: number, isLiked: boolean = false): void {
+    const index = this.liked.indexOf(id);
+    isLiked ? this.liked.push(id) : this.liked.splice(index, 1);
+    localStorage.setItem('liked', this.liked.toString());
+    //Update animal "isAdoption" to false
   }
 }
