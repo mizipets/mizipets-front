f23ebc0975fc77f9b30e513d9172ff3a26099aa5
juliien <julien1105@outlook.fr>
2022-05-11 16:00:56 +0200

    add: home mobile wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { AnimalsListComponent } from './components/animals/animals-list/animals-
 import { HomeComponent } from './components/home/home.component';
 import { AnimalsDetailComponent } from './components/animals/animals-detail/animals-detail.component';
 import { CodeInputModule } from 'angular-code-input';
+import { HomeMobileComponent } from './components/home-mobile/home-mobile.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http);
@@ -38,7 +39,8 @@ export function HttpLoaderFactory(http: HttpClient) {
         RegisterComponent,
         AnimalsListComponent,
         HomeComponent,
-        AnimalsDetailComponent
+        AnimalsDetailComponent,
+        HomeMobileComponent
     ],
     imports: [
         BrowserModule,
