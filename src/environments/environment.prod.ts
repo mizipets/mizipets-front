c8b39a2c5bbccf8e1cfb04d28b4b94d1815df286
Maxime <maxime.dharboulle@gmail.com>
2022-06-25 16:06:58 +0200

    Add: notifications socket env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     baseUrl: 'https://api.mizipets.com/v1/',
     socketUrl: 'https://messages.mizipets.com',
-    notificationsSocketUrl: 'http://localhost:3002',
+    notificationsSocketUrl: 'https://notifications.mizipets.com',
     userDefaultUrl: 'https://storage.mizipets.com/mizipets/avatar_default.jpg'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     baseUrl: 'https://api.staging.mizipets.com/v1/',
     socketUrl: 'https://messages.staging.mizipets.com',
-    notificationsSocketUrl: 'http://localhost:3002',
+    notificationsSocketUrl: 'https://notifications.staging.mizipets.com',
     userDefaultUrl: 'https://storage.staging.mizipets.com/mizipets/avatar_default.jpg'
 };
