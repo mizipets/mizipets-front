54bc5a54e1d352c1795581277d26c168ae40a3f9
juliien <julien1105@outlook.fr>
2022-06-29 02:44:34 +0200

    fix: mobile-home by adding mobile guard

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from "ngx-device-detector";
 
 @Component({
     selector: 'app-home',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    constructor() {}
+    isMobileDevice: boolean = false;
+    constructor(
+      private deviceService: DeviceDetectorService
+    ) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+      this.isMobileDevice =
+        this.deviceService.isMobile() || this.deviceService.isTablet();
+    }
 }
