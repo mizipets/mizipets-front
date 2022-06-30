a49d9419c342fd58e88f76295dc01dd73d796494
juliien <julien1105@outlook.fr>
2022-06-30 15:42:36 +0200

    fix: home page new design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from "ngx-device-detector";
+import { AnimalsService } from "../../services/animals.service";
+import { AnimalModel } from "../../models/animal.model";
 
 @Component({
     selector: 'app-home',
@@ -8,12 +10,15 @@ import { DeviceDetectorService } from "ngx-device-detector";
 })
 export class HomeComponent implements OnInit {
     isMobileDevice: boolean = false;
+    animals: AnimalModel[] = [];
+
     constructor(
-      private deviceService: DeviceDetectorService
+      private deviceService: DeviceDetectorService,
+      private animalService: AnimalsService
     ) {}
 
     ngOnInit(): void {
-      this.isMobileDevice =
-        this.deviceService.isMobile() || this.deviceService.isTablet();
+        this.isMobileDevice = this.deviceService.isMobile() || this.deviceService.isTablet();
+        this.animalService.getFetchedAnimal().subscribe((animals: AnimalModel[]) => this.animals = animals);
     }
 }
